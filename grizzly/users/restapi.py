d07d8505e2f4455b18a55b6de7a905b5d1f76522
Mikael Göransson <github@mgor.se>
2023-11-27 09:24:35 +0100

    geventhttpclient does not support `cookies` in `request`
    add the dict (name, value) cookies in the clients `cookiejar` as a `Cookie` instead.

--- grizzly/users/restapi.py
+++ grizzly/users/restapi.py
@@ -55,8 +55,10 @@ from __future__ import annotations
 import json
 from abc import ABCMeta
 from copy import copy
+from html.parser import HTMLParser
+from http.cookiejar import Cookie
 from time import time
-from typing import TYPE_CHECKING, Any, ClassVar, Dict, Optional, cast
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, List, Optional, Tuple, cast
 
 import requests
 from locust.contrib.fasthttp import FastHttpSession
@@ -77,6 +79,33 @@ if TYPE_CHECKING:  # pragma: no cover
 
 class RestApiUserMeta(GrizzlyUserMeta, ABCMeta):
     pass
+
+
+class HtmlTitleParser(HTMLParser):
+    title: Optional[str]
+
+    _look: bool
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.title = None
+        self._look = False
+
+    @property
+    def should_look(self) -> bool:
+        return self._look and self.title is None
+
+    def handle_starttag(self, tag: str, _attrs: List[Tuple[str, Optional[str]]]) -> None:
+        self._look = tag == 'title' and self.title is None
+
+    def handle_data(self, data: str) -> None:
+        if self.should_look:
+            self.title = data
+
+    def handle_endtag(self, tag: str) -> None:
+        if tag == 'title' and self.should_look:
+            self._look = False
 
 
 @grizzlycontext(context={
@@ -113,7 +142,7 @@ class RestApiUser(GrizzlyUser, AsyncRequests, GrizzlyHttpAuthClient, metaclass=R
         }, self.metadata)
 
         self.session_started = None
-
+        self.cookies = {}
         self.client = FastHttpSession(
             environment=self.environment,
             base_url=self.host,
@@ -125,7 +154,6 @@ class RestApiUser(GrizzlyUser, AsyncRequests, GrizzlyHttpAuthClient, metaclass=R
         )
 
         self.parent = None
-        self.cookies = {}
 
     def on_start(self) -> None:
         super().on_start()
@@ -152,6 +180,12 @@ class RestApiUser(GrizzlyUser, AsyncRequests, GrizzlyHttpAuthClient, metaclass=R
                     message = response.text
             except json.decoder.JSONDecodeError:
                 message = response.text
+
+                parser = HtmlTitleParser()
+                parser.feed(message)
+
+                if parser.title is not None:
+                    message = parser.title.strip()
 
         return message
 
@@ -212,12 +246,33 @@ class RestApiUser(GrizzlyUser, AsyncRequests, GrizzlyHttpAuthClient, metaclass=R
         headers: Optional[Dict[str, str]] = None
         payload: Optional[str] = None
 
+        client.cookiejar.clear()
+
+        for name, value in self.cookies.items():
+            client.cookiejar.set_cookie(Cookie(
+                version=0,
+                name=name,
+                value=value,
+                port=None,
+                port_specified=False,
+                domain=self.host,
+                domain_specified=True,
+                domain_initial_dot=False,
+                path='/',
+                path_specified=True,
+                secure=self.host.startswith('https'),
+                expires=None,
+                discard=False,
+                comment=None,
+                comment_url=None,
+                rest={},
+            ))
+
         with client.request(
             method=request.method.name,
             name=request.name,
             url=url,
             catch_response=True,
-            cookies=self.cookies,
             **parameters,
         ) as response:
             # monkey patch, so we don't get two request events
